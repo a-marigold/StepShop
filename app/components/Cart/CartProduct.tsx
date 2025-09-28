15e7607329094533298637ab4fcab81eedd24178
NextKolya <phelixgluy@gmail.com>
2025-09-28 20:00:02 +0600

    Add CartProduct

--- app/components/Cart/CartProduct.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import EmptyFilledButton from '@UI/EmptyFilledButton';
-
-import cartStyles from './Cart.module.scss';
-
-export default function SearchInput() {
-    return <div></div>;
-}
---
+++ app/components/CartModal/CartProduct.tsx
@@ -0,0 +1,62 @@
+'use client';
+
+import Image from 'next/image';
+
+import EmptyFilledButton from '@UI/EmptyFilledButton';
+
+import productStyles from './CartProduct.module.scss';
+
+interface CartProductProps {
+    image: string;
+
+    name: string;
+    options: string;
+
+    price: number;
+}
+
+export default function CartProduct({
+    image,
+    name,
+    options,
+    price,
+}: CartProductProps) {
+    return (
+        <div className={productStyles['cart-product']}>
+            <Image src={image} alt={name} width={65} height={65} />
+
+            <div className={productStyles['description-block']}>
+                <div className={productStyles['text-block']}>
+                    <p className={productStyles['title']}>{name}</p>
+                    <p className={productStyles['options']}>{options}</p>
+                </div>
+
+                <div className={productStyles['horizontal-line']}></div>
+
+                <div className={productStyles['order-block']}>
+                    <div className={productStyles['quantity-block']}>
+                        <EmptyFilledButton
+                            imageSettings={{
+                                imageUrl: '/images/minus-icon.svg',
+                            }}
+                            classNames={[productStyles['quantity-button']]}
+                            ariaLabel='Убрать один товар'
+                        />
+
+                        <span className={productStyles['quantity']}>1</span>
+
+                        <EmptyFilledButton
+                            imageSettings={{
+                                imageUrl: '/images/plus-icon.svg',
+                            }}
+                            classNames={[productStyles['quantity-button']]}
+                            ariaLabel='Добавить 1 товар'
+                        />
+                    </div>
+
+                    <span className={productStyles['price']}>{price} ₸</span>
+                </div>
+            </div>
+        </div>
+    );
+}
