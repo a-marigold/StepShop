6065582714f77bab9a353e9885682232f42e1210
NextKolya <phelixgluy@gmail.com>
2025-09-28 15:04:09 +0600

    Add not-found general page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,20 +1,33 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-import homeStyles from './Home.module.scss';
+import { Metadata } from 'next';
+
+import notFoundStyles from './NotFound.module.scss';
+
+export const metadata: Metadata = {
+    title: 'Страница не найдена',
+    openGraph: {
+        title: 'Страница не найдена',
+    },
+};
 
 export default function NotFound() {
     return (
-        <main className={homeStyles['not-found-page']}>
-            <div className={homeStyles['not-found-box']}>
-                <div className={homeStyles['not-found-text-box']}>
-                    <h1>Страница не найдена</h1>
-                    <p>
+        <main className={notFoundStyles['not-found-page']}>
+            <div className={notFoundStyles['not-found-box']}>
+                <div className={notFoundStyles['text-box']}>
+                    <h1 className={notFoundStyles['title']}>
+                        Страница не найдена
+                    </h1>
+                    <p className={notFoundStyles['description']}>
                         Проверьте корректность введённого адреса или повторите
                         попытку позже
                     </p>
 
-                    <div className={homeStyles['return-buttons-block']}></div>
+                    <div
+                        className={notFoundStyles['return-buttons-block']}
+                    ></div>
                 </div>
 
                 <Image
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Header from './components/Header/Header';
+import type { Metadata } from 'next';
 
 import Navigation from './components/Navigation/Navigation';
 
@@ -8,6 +8,16 @@ import FiltrationProvider from './components/Filtration/FiltrationProvider';
 import ProductList from './components/ProductList/ProductList';
 
 import homeStyles from './Home.module.scss';
+
+export const metadata: Metadata = {
+    title: 'Каталог товаров интернет магазина StepShop',
+    description:
+        'Красота и здоровье. Дом и сад. Мебель. Аксессуары. Строительство и ремонт. Автотовары. Продукты питания. Товары для животных. Детские товары. Спорт и отдых.',
+
+    openGraph: {
+        title: 'Каталог товаров интернет магазина StepShop',
+    },
+};
 
 export default function Home({ searchParams }: SearchParamsProp) {
     return (
