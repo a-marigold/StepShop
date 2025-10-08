7493c103a0dce92b9b49269b821e6757383a7b49
NextKolya <phelixgluy@gmail.com>
2025-10-08 19:19:47 +0600

    Complete jest.config configuration

--- utils/scrollLock/scrollLock.test.ts
+++ utils/scrollLock/scrollLock.test.ts
@@ -1,6 +1,15 @@
 import { getDocumentScrollbarWidth } from './scrollLock';
 
 describe('Document width calculating test', () => {
+    Object.defineProperty(window, 'innerWidth', {
+        writable: false,
+        value: 1016,
+    });
+    Object.defineProperty(document.documentElement, 'clientWidth', {
+        writable: false,
+        value: 1001,
+    });
+
     it('Should return real scrollbar width of document', () => {
         expect(getDocumentScrollbarWidth()).toBe(15);
     });
