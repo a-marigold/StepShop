a4e9b7f5fe8cab5928d7b32364b43de3b0616b48
NextKolya <phelixgluy@gmail.com>
2025-10-12 21:47:58 +0600

    Fix cors in backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,12 @@
 import Fastify from 'fastify';
 
 import type { ZodTypeProvider } from 'fastify-type-provider-zod';
-
 import {
     validatorCompiler,
     serializerCompiler,
 } from 'fastify-type-provider-zod';
+
+import cors from '@fastify/cors';
 
 import prismaPlugin from './plugins/prisma';
 
@@ -16,10 +17,17 @@ const app = Fastify({
 }).withTypeProvider<ZodTypeProvider>();
 
 export async function buildApp() {
-    app.register(prismaPlugin);
-
     app.setValidatorCompiler(validatorCompiler);
     app.setSerializerCompiler(serializerCompiler);
+
+    app.register(cors, {
+        origin: ['https://step-shop.vercel.app', 'http://localhost:3000'],
+
+        methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+        credentials: true,
+    });
+
+    app.register(prismaPlugin);
 
     app.register(routes);
 
