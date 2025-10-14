0c297b170c79fa8a17cf96e4071a2b41d35b2c86
NextKolya <phelixgluy@gmail.com>
2025-10-14 23:19:58 +0600

    Add Navbar in /admin

--- frontend/app/admin/components/Navbar/Navbar.tsx
+++ frontend/app/admin/components/Navbar/Navbar.tsx
@@ -1,5 +1,36 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+
+import Link from 'next/link';
+
+import clsx from 'clsx';
 import navStyles from './Navbar.module.scss';
 
+const controllersList = [
+    { title: 'Добавить товар', path: '/admin/create' },
+    { title: 'Удалить товар', path: '/admin/delete' },
+    { title: 'Обновить товар', path: '/admin/update' },
+];
+
 export default function Navbar() {
-    return <div className={navStyles['admin-page']}></div>;
+    const pathname = usePathname();
+
+    return (
+        <nav className={navStyles['navbar']}>
+            {controllersList.map((controller, index) => (
+                <Link
+                    key={index}
+                    href={controller.path}
+                    className={clsx(
+                        navStyles['controller'],
+                        controller.title === pathname &&
+                            navStyles['active-controller']
+                    )}
+                >
+                    {controller.title}
+                </Link>
+            ))}
+        </nav>
+    );
 }
