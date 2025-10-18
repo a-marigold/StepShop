a74bb9131be1ad2e8def4b9037037e0c701d72a6
NextKolya <phelixgluy@gmail.com>
2025-10-18 22:52:29 +0600

    Fixes in backend for render.com

--- shared/.dist/types/ApiResponseType.d.ts
+++
@@ -1,9 +0,0 @@
-import z from 'zod';
-export declare const ApiResponseSchema: z.ZodObject<
-    {
-        statusCode: z.ZodNumber;
-        message: z.ZodString;
-    },
-    z.core.$strip
->;
-export type ApiResponseType = z.infer<typeof ApiResponseSchema>;
--- shared/.dist/types/ApiResponseType.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ApiResponseSchema = void 0;
-var zod_1 = require("zod");
-exports.ApiResponseSchema = zod_1.default.object({
-    statusCode: zod_1.default.number(),
-    message: zod_1.default.string(),
-});
