46aecaf2bbd980536cbee38c8f377017897811fe
NextKolya <phelixgluy@gmail.com>
2025-10-18 22:02:43 +0600

    Add port binding for render.com in server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,18 @@
 import { buildApp } from './app';
 
+// Binding port for render.com
+const PORT = Number(process.env.PORT) || 1000;
+
 async function start() {
     const app = await buildApp();
 
     try {
-        await app.listen({ port: 1000, host: '0.0.0.0' });
+        await app.listen({ port: PORT, host: '0.0.0.0' });
 
         console.log('Server is running on local host');
     } catch (error) {
         app.log.error(error);
+
         process.exit(1);
     }
 }
